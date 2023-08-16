2a40a8ae7e54c7511949b0ef6e3bc8dc3e162f71
Juanis86 <65877667+Juanis86@users.noreply.github.com>
2023-08-15 22:00:26 -0300

    Add files via upload

---
+++ marketplace/mkplace/frontend/src/lib/src/main.rs
@@ -0,0 +1,62 @@
+// frontend/src/main.rs
+
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use yew_router::prelude::*;
+
+mod search;
+mod combinations;
+mod combination_detail;
+// ... (otros módulos que puedas tener)
+
+// Define las rutas y componentes correspondientes
+#[derive(Switch, Debug, Clone)]
+pub enum AppRoute {
+    #[to = "/combinations"]
+    Combinations,
+    #[to = "/combinations/{id}"]
+    CombinationDetail(String),
+    #[to = "/search"]
+    Search,
+    // ... (otras rutas que puedas necesitar)
+}
+
+pub struct Model;
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        // Lógica de actualización si es necesario
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <Router<AppRoute, ()>
+                render = Router::render(|switch: AppRoute| {
+                    match switch {
+                        AppRoute::Combinations => html!{<combinations::Combinations />},
+                        AppRoute::CombinationDetail(id) => html!{<combination_detail::CombinationDetail id=id />},
+                        AppRoute::Search => html!{<search::Search />},
+                        // ... (manejo de otras rutas)
+                    }
+                })
+            />
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
+}
